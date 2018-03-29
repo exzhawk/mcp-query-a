78ba769d74414ff3e38def86b2b1b630ad64675f
Epix <git@exz.me>
2018-03-30 02:25:58 +0800

    transform param

--- src/app/parser.ts
+++ src/app/parser.ts
@@ -2,18 +2,19 @@ import {FD, MD} from './mcp';
 import * as Papa from 'papaparse/papaparse.min.js';
 
 export class Parser {
-  static java_type = {
-    Z: 'boolean',
-    B: 'byte',
-    C: 'char',
-    S: 'short',
-    I: 'int',
-    J: 'long',
-    F: 'float',
-    D: 'double',
-    V: 'void',
-  };
+  static javaTypesMapping = new Map<string, string>([
+    ['Z', 'boolean'],
+    ['B', 'byte'],
+    ['C', 'char'],
+    ['S', 'short'],
+    ['I', 'int'],
+    ['J', 'long'],
+    ['F', 'float'],
+    ['D', 'double'],
+    ['V', 'void'],
+  ]);
   static paramRe = new RegExp('\\[*(?:L[^;]*;|[ZBCSIJFDV])', 'g');
+  static arrayDetectRe = new RegExp('^(\\[*)(.*)$');
 
   static parse(joined_exc: string, joined_srg: string, fields_csv: string, methods_csv: string, params_csv: string) {
     const fieldsMap = new Map<string, string[]>();
@@ -22,10 +23,10 @@ export class Parser {
       .map(line => {
         const [srg, name, side, desc] = line.split(',');
         fieldsMap.set(srg, [name, side, desc]);
-        if (srg === 'func_99999_d') {
-          console.log(line);
-        }
       });
+    Papa.parse(fields_csv, {header: true})['data'].forEach((d) => {
+      fieldsMap.set(d['searge'], [d['name'], d['side'], d['desc']]);
+    });
     const methodsMap = new Map<string, string[]>();
     Papa.parse(methods_csv, {header: true})['data'].forEach((d) => {
       methodsMap.set(d['searge'], [d['name'], d['side'], d['desc']]);
@@ -42,7 +43,9 @@ export class Parser {
     const constructors = joined_exc.split('\r\n')
       .map(line => constructorRe.exec(line))
       .filter(a => a !== null)
-      .map(a => new MD('', '', a[2], 0, '', a[1].replace(/\//g, '.'), this.parseParam(a[3]), 'V', true));
+      .map(a => new MD('', '', a[2], 0, '',
+        a[1].replace(/\//g, '.'), this.parseParams(a[3]),
+        this.transformType('V'), true));
 
     const joined_srg_lines = joined_srg.split('\r\n');
     const mdRe = new RegExp('^MD: (.*) \\(.*\\).* (.*) \\((.*)\\)(.*)$');
@@ -62,38 +65,55 @@ export class Parser {
         if (fdInfo === undefined) {
           fields.push(new FD(fdA[1].replace(/\//g, '.'), srg, srg, 0, '', path));
         } else {
-          fields.push(new FD(fdA[1].replace(/\//g, '.'), srg, fdInfo[0], fdInfo[1], fdInfo[2], path));
+          fields.push(new FD(fdA[1].replace(/\//g, '.'), srg, fdInfo[0], parseInt(fdInfo[1], 10), fdInfo[2], path));
         }
       } else if (line.startsWith('MD')) {
         const mdA = mdRe.exec(line);
         const pathA = mdA[2].split('/');
         const srg = pathA[pathA.length - 1];
         const path = pathA.slice(0, -1).join('.');
-        const params = this.parseParam(mdA[3]);
+        const params = this.parseParams(mdA[3]);
         const mdInfo = methodsMap.get(srg);
         if (syntheticMdRe.exec(srg)) {
           continue;
         }
 
         if (mdInfo === undefined) {
-          methods.push(new MD(mdA[1].replace(/\//g, '.'), srg, srg, 0, '', path, params, mdA[4], false));
+          methods.push(
+            new MD(mdA[1].replace(/\//g, '.'),
+              srg, srg, 0, '', path, params, this.transformType(mdA[4]), false)
+          );
         } else {
-          methods.push(new MD(mdA[1].replace(/\//g, '.'), srg, mdInfo[0], mdInfo[1], mdInfo[2], path, params, mdA[4], false));
+          methods.push(
+            new MD(mdA[1].replace(/\//g, '.'),
+              srg, mdInfo[0], parseInt(mdInfo[1], 10), mdInfo[2], path, params, this.transformType(mdA[4]), false)
+          );
         }
       }
     }
     return methods.sort(this.sortBySrg);
-
   }
 
-  static parseParam(paramString: string): string[] {
-    if (paramString.length === 0) {
+  static parseParams(paramsString: string): string[] {
+    if (paramsString.length === 0) {
       return [];
     }
-    return paramString.match(this.paramRe);
+    return paramsString.match(this.paramRe).map(s => this.transformType(s));
   }
 
   static sortBySrg(a, b): number {
     return (a.srg < b.srg) ? -1 : (a.srg > b.srg) ? 1 : 0;
+  }
+
+  static transformType(paramString: string): string {
+    let transformed = '';
+    const [, arrayD, rest] = this.arrayDetectRe.exec(paramString);
+    transformed += '[]'.repeat(arrayD.length);
+    if (this.javaTypesMapping.has(rest)) {
+      transformed += this.javaTypesMapping.get(rest);
+    } else if (rest.startsWith('L') && rest.endsWith(';')) {
+      transformed += rest.slice(1, -1).replace(/\//g, '.');
+    }
+    return transformed;
   }
 }
